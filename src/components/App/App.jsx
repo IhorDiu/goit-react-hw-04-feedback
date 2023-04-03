5ddbfb583d4a91ed78f507169b24f2800bda1817
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-04-03 17:30:36 +0300

    without useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 // import { FeedbackWidget } from './FeedbackWidget/FeedbackWidget';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Section } from '../Section/Section';
 import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
@@ -10,8 +10,6 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [countTotalFeedback, setCountTotalFeedback] = useState(0);
-  const [positivePercentage, setPositivePercentage] = useState(0);
 
   const addFeedback = type => {
     switch (type) {
@@ -29,10 +27,13 @@ export const App = () => {
     }
   };
 
-  useEffect(() => {
-    setCountTotalFeedback(() => good + neutral + bad);
-    setPositivePercentage(() => Math.round((good / countTotalFeedback) * 100));
-  }, [good, neutral, bad, countTotalFeedback]);
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const positivePercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
 
   return (
     <div className="FeedbackWidget">
@@ -43,13 +44,13 @@ export const App = () => {
       </Section>
 
       <Section title="Statistics">
-        {countTotalFeedback ? (
+        {countTotalFeedback() ? (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={countTotalFeedback}
-            positivePercentage={positivePercentage}
+            total={countTotalFeedback()}
+            positivePercentage={positivePercentage()}
           />
         ) : (
           <Notification message="There is no feedback" />
